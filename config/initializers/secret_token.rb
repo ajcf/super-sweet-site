12bbe1a4b254db5f6b17f3c1faa09ae951fd73d2
Amanda Cadwell-Frost <acadwellfrost@gmail.com>
2013-06-26 21:18:28 -0400

    initial scaffolds created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SuperSweetSite::Application.config.secret_key_base = '3161089639b5504f956c2c7164c0c38e6c7e22acb73002f2f18e65472ff703b68921ee8ed224bc934df26604dcca5e491bc66988dbb8ef5e621daa0dba969ea9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SuperSweetSite::Application.config.session_store :cookie_store, key: '_super-sweet-site_session'
